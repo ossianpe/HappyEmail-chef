aca02089c8f0c2d0208578a7d6cda6a6eb84ebd1
Peter Ossian <msuossianpe@gmail.com>
2015-01-03 22:58:02 -0500

    Initial Release

---
+++ cookbooks/xslt/metadata.rb
@@ -0,0 +1,14 @@
+name             "xslt"
+maintainer       "FindsYou Limited"
+maintainer_email "developers@findsyou.com"
+license          "Apache 2.0"
+description      "Installs libxslt with development packages"
+version          "0.0.1"
+
+depends "build-essential"
+
+recipe "xslt", "Installs libxslt with development packages"
+
+%w{ centos redhat suse fedora ubuntu debian gentoo }.each do |os|
+  supports os
+end
